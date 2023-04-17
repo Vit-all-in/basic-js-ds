acb61e74deee9278975b2ce7a751922a51185939
Vit-all-in <bulatkin.vitalik@mail.ru>
2023-04-17 09:39:58 +0300

    feat:solution added

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,154 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const {
+  Node
+} = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
 
+  constructor() {
+    this.tree = null;
+  };
+  
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.tree = addWithin(this.tree, data)
+
+    function addWithin(node, value) {
+      if (!node) {
+        return new Node(value)
+      }
+
+      if (node.data === value) {
+        return node;
+      }
+
+      if (value < node.data) {
+        node.left = addWithin(node.left, value)
+      } else {
+        node.right = addWithin(node.right, value)
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.tree, data);
+
+    function searchWithin(node, value) {
+      if (!node) {
+        return false
+      }
+
+      if (node.data === value) {
+        return true
+      }
+
+      return data < node.data ?
+        searchWithin(node.left, value) :
+        searchWithin(node.right, value)
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.tree, data);
+
+    function findNode(node, value) {
+      if (!node) {
+        return null
+      };
+      if (node.data === value) {
+        return node
+      };
+      if (value < node.data) {
+        return findNode(node.left, value)
+      } else {
+        return findNode(node.right, value)
+      };
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.tree = removeNode(this.tree, data);
+
+    function removeNode(node, value) {
+      if (!node) {
+        return null;
+      }
+
+      if (value < node.data) {
+        node.left = removeNode(node.left, value)
+        return node
+      } else if (node.data < value) {
+        node.right = removeNode(node.right, value)
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null
+        }
+      }
+
+      if (!node.left) {
+        node = node.right;
+        return node;
+      }
+
+      if (!node.right) {
+        node = node.left;
+        return node;
+      }
+
+      let minFromRight = node.right;
+      while (minFromRight.left) {
+        minFromRight = minFromRight.left;
+      }
+
+      node.data = minFromRight.data;
+
+      node.right = removeNode(node.right, minFromRight.data);
+
+      return node;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return findMin(this.tree);
+
+    function findMin(node) {
+      if (!node) {
+        return null
+      };
+      if (!node.left) {
+        return node.data
+      };
+
+      return findMin(node.left);
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return findMax(this.tree);
+
+    function findMax(node) {
+      if (!node) {
+        return null
+      };
+      if (!node.right) {
+        return node.data
+      };
+
+      return findMax(node.right);
+    }
   }
 }
 
